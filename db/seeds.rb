baefed84b16fac4a742d587c390a7ff58e33f392
dominaki <dominaki@gmail.com>
2015-08-27 23:46:56 +0300

    wow

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+Store.create(name: 'Whole Foods', location: 'South Loop'
+Store.create(name:  'Trader Joe's, location: 'South Loop')
+
+	# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
